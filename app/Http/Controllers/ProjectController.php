82eda68b485ca002992964cea5307bca86efe137
Danil1325 <danil.strelciuc@gmail.com>
2024-03-26 21:18:26 +0200

    Add edit update and destroy function (:

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use App\Models\Status;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ProjectController extends Controller
 {
@@ -49,24 +50,27 @@ class ProjectController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Project $project)
+    public function edit(Project $project):View
     {
-        //
+        $status = Status::All();
+        return view('projects.edit', ['project' => $project], ['status' => $status]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Project $project)
+    public function update(Request $request, Project $project):RedirectResponse
     {
-        //
+        $project->update($request->all());
+        return redirect()->back()->with('success', 'Actualizare cu success!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Project $project)
+    public function destroy(Project $project):RedirectResponse
     {
-        //
+        $project->delete();
+        return redirect()->back();
     }
 }
